fe7fc173daa4aa37b3e77580077ef2002c296e5e
dev.yoon105 <dev.yoon105@gmail.com>
2022-05-14 02:26:32 +0900

    baekjoon 25083

---
+++ AlgorithmPractice/AlgorithmPractice/25083.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  AlgorithmPractice
+//
+//  Created by 김동윤 on 2022/05/14.
+//
+
+//25083
+
+import Foundation
+
+//8m
+print("""
+         ,r'\"7
+r`-_   ,'  ,/
+ \\. \". L_r'
+   `~\\/
+      |
+      |
+""")
+
+//8m
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\\/")
+print("      |")
+print("      |")

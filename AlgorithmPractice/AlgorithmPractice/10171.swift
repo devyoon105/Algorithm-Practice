01e60a49e48452fb113d403dfdf0b9f86f629d31
dev.yoon105 <dev.yoon105@gmail.com>
2022-05-14 02:20:41 +0900

    baekjoon 10171

---
+++ AlgorithmPractice/AlgorithmPractice/10171.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  AlgorithmPractice
+//
+//  Created by 김동윤 on 2022/05/14.
+//
+
+//10171
+
+import Foundation
+
+//12m
+print("""
+\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|
+""")
+
+//8m
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
